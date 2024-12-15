c2a77b87f785fa3a7abaf024233c64c887af0d9c
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-16 03:12:13 +0900

    feat: app.ts and server.ts setups

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,20 @@
-import dotenv from "dotenv"
-dotenv.config()
+
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import app from "./app";
+
+console.log("PORT:", process.env.PORT);
+console.log("MONGO_URL:", process.env.MONGO_URL);
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("Connected to MongoDB successfully ");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.info(`The server is running successfully on port: ${PORT}`);
+      console.info(`Admin project  https://localhost:${PORT} \n`);
+    });
+  })
+  .catch((err) => console.log("ERROR on connecting to MongoDB", err));
