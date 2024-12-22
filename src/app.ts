45137f8a67988fde9f718ccbe4ff991cc446c267
MukhammadZiya <mukhammadziyaa@gmail.com>
2024-12-23 06:33:20 +0900

    feat : integration of sessions

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,40 @@
-import express from "express"
-import path from "path"
-import router from "./router"
-import routerAdmin from "./routerAdmin"
-import morgan from "morgan"
-import { MORGAN_FORMAT } from "./libs/config"
+import express from "express";
+import path from "path";
+import router from "./router";
+import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
 
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
 
-const app = express()
-app.use(express.static(path.join(__dirname, "public")))
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(morgan(MORGAN_FORMAT))
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000* 3600 * 60,
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
+app.use("/admin", routerAdmin);
+app.use("/", router);
 
-
-
-app.use("/admin", routerAdmin)
-app.use("/", router)
-
-
-
-export default app
+export default app;
