2c733790ba99eebd6293ef90e7ab8530e8798636
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-01-05 01:02:06 +0900

    feat: develop spa login api logic

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -7,7 +7,7 @@ import {
 } from "../libs/types/member";
 import MemberModel from "../schema/Member.model";
 import { HttpCode } from "../libs/Errors";
-import { MemberType } from "../libs/enums/member.enum";
+import { MemberStatus, MemberType } from "../libs/enums/member.enum";
 import * as bcrypt from "bcryptjs";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 
@@ -39,12 +39,18 @@ class MemberService {
   public async login(input: LoginInput): Promise<Member> {
     const member = await this.memberModel
       .findOne(
-        { memberNick: input.memberNick },
-        { memberNick: 1, memberPassword: 1 }
+        {
+          memberNick: input.memberNick,
+          memberStatus: { $ne: MemberStatus.DELETE },
+        },
+        { memberNick: 1, memberPassword: 1, memberStatus: 1 }
       )
       .exec();
 
     if (!member) throw new Errors(HttpCode.BAD_REQUEST, Message.NO_MEMBER_NICK);
+    else if (member.memberStatus === MemberStatus.BLOCK) {
+      throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
+    }
 
     const isMatch = await bcrypt.compare(
       input.memberPassword,
@@ -64,7 +70,6 @@ class MemberService {
       .exec();
 
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
-
 
     console.log("after", input.memberPassword);
     const salt = await bcrypt.genSalt();
