521229769c1c3ddc1683db7fbd4d0f2a4026cd36
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-01-04 18:25:47 +0900

    feat: develop users update with real data

--- src/public/js/users.js
+++ src/public/js/users.js
@@ -1,1 +1,27 @@
 console.log("Users frontend javascript file");
+
+$(function () {
+  $(".member-status").on("change", function (e) {
+    const id = e.target.id,
+      memberStatus = $(`#${id}.member-status`).val();
+
+    axios
+      .post("/admin/user/edit", {
+        _id: id,
+        memberStatus: memberStatus,
+      })
+      .then((response) => {
+        console.log("response:", response);
+        const result = response.data;
+
+        if (result.data) {
+          console.log("User updated!!!");
+          $(".member-status").blur();
+        } else alert("User update failed");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("User update failed");
+      });
+  });
+});
