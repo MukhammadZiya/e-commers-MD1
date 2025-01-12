9beda9c914b082e92fed274cd51a6fa4d71b3c5e
MukhammadZiya <mukhammadziyaa@gmail.com>
2025-01-13 05:15:58 +0900

    feat: develop getProducts rest API

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,4 +1,4 @@
-import { ProductInput, ProductUpdateInput } from "../libs/types/product";
+import { ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { Product } from "../libs/types/product";
 import { HttpCode } from "../libs/Errors";
@@ -6,6 +6,8 @@ import { Message } from "../libs/Errors";
 import Errors from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import { Member } from "../libs/types/member";
+import { T } from "../libs/types/common";
+import { ProductStatus } from "../libs/enums/product.enum";
 
 class ProductService {
   private readonly productModel;
@@ -14,6 +16,35 @@ class ProductService {
     this.productModel = ProductModel;
   }
   /** SPA */
+
+  public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+    const match: T = { productStatus: ProductStatus.PROCESS };
+
+    if (inquiry.productCollection)
+      match.productCollection = inquiry.productCollection;
+
+    if (inquiry.search) {
+      match.productName = { $regex: new RegExp(inquiry.search, "i") };
+    }
+
+    const sort: T =
+      inquiry.order === "productPrice"
+        ? { [inquiry.order]: 1 }
+        : { [inquiry.order]: -1 };
+
+    const result = await this.productModel
+      .aggregate([
+        { $match: match },
+        { $sort: sort },
+        { $skip: (inquiry.page * 1 - 1) * inquiry.limit },
+        { $limit: inquiry.limit * 1 },
+      ])
+      .exec();
+
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+  }
 
   /** SSR */
 
